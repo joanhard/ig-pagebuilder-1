e702f058ae5e597771ae322ea69f85ae1426658c
innogears <innogears@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-25 11:28:01 +0000

    release version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/ig-pagebuilder/trunk@864652 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ig-pagebuilder.php
+++ ig-pagebuilder.php
@@ -3,7 +3,7 @@
  * Plugin Name: IG Pagebuilder
  * Plugin URI:  http://www.innogears.com
  * Description: Awesome content builder for Wordpress websites
- * Version:     1.0.9
+ * Version:     2.0
  * Author:      InnoGears Team <support@www.innogears.com>
  * Author URI:  http://www.innogears.com
  * License:     GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
@@ -19,22 +19,21 @@ if ( ! class_exists( 'IG_Pb_Init' ) ) {
 
 			$this->includes();
 			$this->autoload();
+			$this->cls_alias();
 
-			global $pagenow;
-			$pages = array( 'post.php', 'post-new.php', 'widgets.php', 'admin-ajax.php' );
-			if ( in_array( $pagenow, $pages )  || IG_Pb_Helper_Functions::is_modal() || ! is_admin() ) {
-				// Init the plugin
-				IG_Pb_Plugin::init();
-				// Assets load management
-				IG_Pb_Assets::hook();
-			}
+			// Init the plugin
+			IG_Pb_Assets_Register::init();
+			// Assets load management
+			IG_Pb_Assets_Load::hook();
 
 			add_action( 'widgets_init', array( &$this, 'init' ), 100 );
 		}
 
 		function init(){
 			global $Ig_Pb, $Ig_Pb_Widgets;
-			$Ig_Pb = new IG_Pb();
+			$Ig_Pb = new IG_Pb_Core();
+			$IG_Pb_Utils_Plugin = new IG_Pb_Utils_Plugin();
+			IG_Product_Plugin::init();
 			$Ig_Pb_Widgets = ! empty( $Ig_Pb_Widgets ) ? $Ig_Pb_Widgets : IG_Pb_Helper_Functions::widgets();
 		}
 
@@ -42,23 +41,22 @@ if ( ! class_exists( 'IG_Pb_Init' ) ) {
 		function includes() {
 			// include core files
 			include_once 'core/loader.php';
-			include_once 'core/pb.php';
-			include_once 'core/third-party.php';
-			// include utility files
-			include_once 'core/utils/common.php';
-			include_once 'core/utils/placeholder.php';
-			include_once 'core/utils/plugin.php';
+			include_once 'core/defines.php';
 		}
 
 		// autoload register
 		function autoload(){
-			IG_Pb_Loader::register( IG_PB_PATH . 'core/assets', 'IG_Pb_' );
-			IG_Pb_Loader::register( IG_PB_PATH . 'core/helper', 'IG_Pb_Helper' );
-			IG_PB_Loader::register( IG_PB_PATH . 'core/helper/html', 'IG_Pb_Helper_Html_' );
-			IG_Pb_Loader::register( IG_PB_PATH . 'core/objects', 'IG_Pb_' );
-			IG_Pb_Loader::register( IG_PB_PATH . 'core/shortcode', 'IG_Pb_' );
+			IG_PB_Loader::register( IG_PB_PATH . 'core', 'IG_Pb_' );
+			IG_PB_Loader::register( IG_PB_PATH . 'core/product', 'IG_Product_' );
 
 			do_action( 'ig_pb_autoload' );
+		}
+
+		// For revoking class from library without changes
+		function cls_alias() {
+			class_alias( 'IG_Pb_Loader', 'IG_Loader' );
+			class_alias( 'IG_Pb_Utils_Admin_Menu', 'IG_Init_Admin_Menu' );
+			class_alias( 'IG_Pb_Assets_Load', 'IG_Init_Assets' );
 		}
 	}
 
